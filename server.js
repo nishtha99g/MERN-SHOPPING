609bd2d76a432779ff2f0d0e708228492a59b756
nishtha99 <nishtha99g@gmail.com>
2020-04-12 16:34:02 +0530

    edit to host project

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express=require('express');
 const mongoose=require('mongoose');
 const bodyParser = require('body-parser');
 const items=require('./routes/api/items');
+const path=require('path');
 const app=express();
 //body parser middleware
 app.use(bodyParser.json());
@@ -14,7 +15,13 @@ mongoose
                     .catch(err=>console.log(err));
                     app.use('/api/items',items);
 
-//serve static assests
-
+//serve static assests if in production
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*'',(req,res)=>{
+res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+});
+}
 const port=process.env.PORT||5000;
-app.listen(port,()=>console.log(`server s
+app.listen(port,()=>console.log(`server started on port ${port}`));
